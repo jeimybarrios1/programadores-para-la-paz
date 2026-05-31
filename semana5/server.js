a1b103f2c0044d0a35f96dfb2b6168bfed3659a5
jeimybarrios1 <jeimybarriosestrada@gmail.com>
2026-05-30 23:33:19 -0500

    Update server.js

--- semana5/server.js
+++ semana5/server.js
@@ -16,6 +16,14 @@ app.get('/mensaje/:nombre', (req, res) => {
   res.send('Hola ' + nombre);
 });
 
+app.post('/reporte', (req, res) => {
+  const mensaje = req.body.mensaje;
+  res.json({
+    estado: "Reporte recibido",
+    mensaje: mensaje
+  });
+});
+
 app.listen(3000, () => {
   console.log('Servidor ejecutándose en puerto 3000');
 });
